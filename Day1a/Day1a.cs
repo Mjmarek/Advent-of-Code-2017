ad052af0a350dd2dffa2ab04845e8b95c16bd1d2
Mjmarek <monicamarek24@gmail.com>
2017-12-03 10:28:51 -0500

    Test 1 green for Day 1a challenge

---
+++ Day1a/Day1a.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//for each item in array, compare to following item
+//if they match, add that number to solution
+//last number should wrap around to front for evaluation
+
+namespace Solutions
+{
+    public class Day1a
+    {
+        public object DigitCount(string input)
+        {
+            var solution = 0;
+            for (var i = 0; i < (input.Length - 1); i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    var num = (input[i]).ToString();
+                    solution = int.Parse(num) + solution;
+                }
+            };
+            return solution;
+        }
+    }
+}
---
+++ Solutions.test/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions.test
+{
+    [TestClass]
+    public class Day1aTest
+    {
+        [TestMethod]
+        public void ShouldReturn3For1122()
+        {
+            string input = "1122";
+
+            var target = new Day1a();
+            var result = target.DigitCount(input);
+
+            Assert.AreEqual(3, result, "First example input should result in 3.");
+        }
+
+        [TestMethod]
+        public void ShouldReturn4For1111()
+        {
+            string input = "1111";
+
+            var target = new Day1a();
+            var result = target.DigitCount(input);
+
+            Assert.AreEqual(4, result, "First example input should result in 4.");
+        }
+    }
+}
