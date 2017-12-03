ad7a26c87ec6fba41cfae409587128a87c155bf8
Mjmarek <monicamarek24@gmail.com>
2017-12-03 13:32:55 -0500

    Day2a Challenge solution

---
+++ Day1a/Day2a.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace Solutions
+{
+    public class Day2a
+    {
+        private string input;
+
+        public Day2a(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var checksum = 0;
+
+            var inputByLines = input.Split('\n');
+            foreach (var line in inputByLines)
+            {
+                var cells = line.Split(' ', '\t');
+                var cellsAsInts = cells.Select(cell => int.Parse(cell)).ToList();
+                var smallest = cellsAsInts.Min();
+                var largest = cellsAsInts.Max();
+                var difference = largest - smallest;
+                checksum += difference;
+            }
+
+            return checksum;
+        }
+    }
+}
