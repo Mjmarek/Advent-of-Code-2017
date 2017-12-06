f115a66d9c4423c0c7ba22762c0ac7cb7e6b643c
Mjmarek <monicamarek24@gmail.com>
2017-12-05 22:50:59 -0500

    Day4a Challenge Solution

---
+++ Solutions/Day4a.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day4a
+    {
+        private string input;
+
+        public Day4a(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var validPassphraseCount = 0;
+
+            var inputByLines = input.Split('\n');
+            foreach (var line in inputByLines)
+            {
+                var words = line.Split(' ').Select(word => word.Trim());
+
+                if (words.Distinct().Count() == words.Count())
+                {
+                    validPassphraseCount += 1;
+                }
+            }
+            return validPassphraseCount;
+        }
+    }
+}
