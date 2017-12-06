39085ab19e9070bdf355dd6ab7c58dfbcfa9693e
Mjmarek <monicamarek24@gmail.com>
2017-12-05 22:04:21 -0500

    Day3b Challenge Solution

--- Solutions.test/Day3bTest.cs
+++ Solutions.test/Day3bTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges;
 
 namespace Solutions.test
 {
@@ -7,8 +8,30 @@ namespace Solutions.test
     public class Day3bTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturn2For1()
         {
+            var input = 1;
+            var result = new Day3b(input).Solve();
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturn23For11()
+        {
+            var input = 11;
+            var result = new Day3b(input).Solve();
+
+            Assert.AreEqual(23, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResult()
+        {
+            var input = 289326;
+            var result = new Day3b(input).Solve();
+
+            Assert.AreEqual(295229, result);
         }
     }
 }
--- Solutions/Day3b.cs
+++ Solutions/Day3b.cs
@@ -8,5 +8,82 @@ namespace Challenges
 {
     public class Day3b
     {
+        public static readonly int GRID_SIZE = 100;
+
+        public Dictionary<int, Dictionary<int, int>> Grid { get; set; }
+
+        private int input;
+
+        public Day3b(int input)
+        {
+            this.input = input;
+
+            Grid = new Dictionary<int, Dictionary<int, int>>();
+
+            foreach (var xCoordinate in Enumerable.Range(-(GRID_SIZE / 2), GRID_SIZE))
+            {
+                Grid[xCoordinate] = new Dictionary<int, int>();
+
+                foreach (var yCoordinate in Enumerable.Range(-(GRID_SIZE / 2), GRID_SIZE))
+                {
+                    Grid[xCoordinate][yCoordinate] = 0;
+                }
+            }
+            Grid[0][0] = 1;
+        }
+
+        public int Solve()
+        {
+            var x = 0;
+            var y = 0;
+            var boundary = 1;
+
+            var direction = Direction.Right;
+
+            while (true)
+            {
+                switch (direction)
+                {
+                    case Direction.Right:
+                        x++;
+                        if (x == boundary && y == -boundary)
+                        {
+                            boundary++;
+                        }
+                        else if (x == boundary) direction = Direction.Up;
+                        break;
+
+                    case Direction.Up:
+                        y++;
+                        if (y == boundary) direction = Direction.Left;
+                        break;
+
+                    case Direction.Left:
+                        x--;
+                        if (x == -boundary) direction = Direction.Down;
+                        break;
+
+                    case Direction.Down:
+                        y--;
+                        if (y == -boundary) direction = Direction.Right;
+                        break;
+                }
+
+                var squareValue = 0;
+                squareValue = Grid[x + 1][y] + squareValue;
+                squareValue = Grid[x + 1][y + 1] + squareValue;
+                squareValue = Grid[x][y + 1] + squareValue;
+                squareValue = Grid[x - 1][y + 1] + squareValue;
+                squareValue = Grid[x - 1][y] + squareValue;
+                squareValue = Grid[x - 1][y - 1] + squareValue;
+                squareValue = Grid[x][y - 1] + squareValue;
+                squareValue = Grid[x + 1][y - 1] + squareValue;
+
+                Grid[x][y] = squareValue;
+
+                if (squareValue > input) return squareValue;
+            }
+            throw new Exception("Nope.");
+        }
     }
 }
