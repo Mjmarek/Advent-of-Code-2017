6e127a8e1c408d9eafccdbb78e8e56e9cef287ce
Mjmarek <monicamarek24@gmail.com>
2017-12-07 22:00:38 -0500

    Day5a & Day5b Challenge Sollutions

---
+++ Solutions/Day5a.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day5a
+    {
+        private string input;
+
+        public Day5a(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var steps = 0;
+
+            var jumps = input.Split('\n').Select(jump => int.Parse(jump.Trim())).ToArray();
+
+            var currentPosition = 0;
+
+            while(currentPosition < jumps.Length)
+            {
+                var currentValue = 0;
+
+                currentValue = jumps[currentPosition];
+
+                jumps[currentPosition] = currentValue + 1;
+
+                currentPosition += currentValue;
+
+                steps += 1;
+            }
+
+            return steps;
+        }
+    }
+}
---
+++ Solutions/Day5b.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day5b
+    {
+        private string input;
+
+        public Day5b(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var steps = 0;
+
+            var jumps = input.Split('\n').Select(jump => int.Parse(jump.Trim())).ToArray();
+
+            var currentPosition = 0;
+
+            while (currentPosition < jumps.Length)
+            {
+                var currentValue = 0;
+
+                currentValue = jumps[currentPosition];
+
+                if (currentValue >= 3)
+                {
+                    jumps[currentPosition] = currentValue - 1;
+                }
+
+                else jumps[currentPosition] = currentValue + 1;
+
+                currentPosition += currentValue;
+
+                steps += 1;
+            }
+
+            return steps;
+        }
+    }
+}
