fc61242a4df79b2accde8e4caa020b9fd3b82512
Mjmarek <monicamarek24@gmail.com>
2017-12-09 11:27:45 -0500

    Day 6b challenge solution

---
+++ Solutions.test/Day6bTest.cs
@@ -0,0 +1,44 @@
+﻿using Challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.test
+{
+    [TestClass]
+    public class Day6bTest
+    {
+        [TestMethod]
+        public void ShouldReturn4()
+        {
+            var input = @"0 2 7 0";
+
+            var result = new Day6b(input).Solve();
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultMonica()
+        {
+            var input = @"0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11";
+
+            var result = new Day6b(input).Solve();
+
+            Assert.AreEqual(1695, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultRyan()
+        {
+            var input = @"10	3	15	10	5	15	5	15	9	2	5	8	5	2	3	6";
+
+            var result = new Day6b(input).Solve();
+
+            Assert.AreEqual(2765, result);
+        }
+    }
+}
---
+++ Solutions/Day6b.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day6b
+    {
+        private string input;
+
+        public Day6b(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var cycles = 0;
+
+            var blocks = input.Split(' ','\t').Select(block => int.Parse(block.Trim())).ToList();
+
+            var states = new List<string>();
+            var didSeeFirstCycle = false;
+
+            var initialState = String.Join(" ", blocks.Select(block => block.ToString()));
+
+            states.Add(initialState);
+
+            while (true)
+            {
+                var maxValue = blocks.Max();
+                var maxValueIndex = blocks.IndexOf(maxValue);
+
+                blocks[maxValueIndex] = 0;
+                var blockIndex = maxValueIndex == blocks.Count - 1 ? 0 : maxValueIndex + 1;
+                for (int i = 0; i < maxValue; i++)
+                {
+                    blocks[blockIndex] += 1;
+
+                    if (blockIndex == blocks.Count - 1) blockIndex = 0;
+                    else blockIndex++;
+                }
+                cycles += 1;
+
+                var currentState = String.Join(" ", blocks.Select(block => block.ToString()));
+
+                if (states.Contains(currentState)) {
+                    if (didSeeFirstCycle)
+                    {
+                        return cycles;
+                    }
+                    states = new List<string>();
+                    didSeeFirstCycle = true;
+                    cycles = 0;
+                }
+                states.Add(currentState);
+            }
+            return cycles;
+        }
+    }
+}
