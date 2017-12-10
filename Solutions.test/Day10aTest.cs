db71146dd364a70e7a3b9b3636e19b332c7b0de1
Mjmarek <monicamarek24@gmail.com>
2017-12-10 17:17:51 -0500

    Day10a Challenge Solution

---
+++ Solutions.test/Day10aTest.cs
@@ -0,0 +1,47 @@
+﻿using Challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.test
+{
+    [TestClass]
+    public class Day10aTest
+    {
+        [TestMethod]
+        public void ShouldReturn12()
+        {
+            var inputElementCount = 5;
+            var inputLengths = "3,4,1,5";
+
+            var result = new Day10a(inputElementCount, inputLengths).Solve();
+
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultMonica()
+        {
+            var inputElementCount = 256;
+            var inputLengths = "212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164";
+
+            var result = new Day10a(inputElementCount, inputLengths).Solve();
+
+            Assert.AreEqual(212, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultRyan()
+        {
+            var inputElementCount = 256;
+            var inputLengths = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24";
+
+            var result = new Day10a(inputElementCount, inputLengths).Solve();
+
+            Assert.AreEqual(4480, result);
+        }
+    }
+}
---
+++ Solutions/Day10a.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day10a
+    {
+        private int inputElementCount = 5;
+        private string inputLengths;
+
+        public Day10a(int inputElementCount, string inputLengths)
+        {
+            this.inputElementCount = inputElementCount;
+            this.inputLengths = inputLengths;
+        }
+
+        public int Solve()
+        {
+            var lengths = inputLengths.Split(',').Select(length => int.Parse(length)).ToArray();
+            var elements = Enumerable.Range(0, inputElementCount).ToArray();
+
+            var currentPosition = 0;
+            var skipSize = 0;
+
+            foreach (var length in lengths)
+            {
+                var reverseList = new List<int>();
+
+                var currentReverseListPosition = currentPosition;
+                for (int i = 0; i < length; i++)
+                {
+                    if (currentReverseListPosition >= elements.Length)
+                    {
+                        currentReverseListPosition = 0;
+                    }
+                    reverseList.Add(elements[currentReverseListPosition]);
+
+                    currentReverseListPosition++;
+                }
+
+                for (int i = length-1; i >= 0; i--)
+                {
+                    if (currentPosition >= elements.Length)
+                    {
+                        currentPosition = 0;
+                    }
+                    elements[currentPosition] = reverseList[i];
+
+                    currentPosition++;
+                }
+
+                if (currentPosition + skipSize >= elements.Length)
+                {
+                    currentPosition = currentPosition + skipSize - elements.Length;
+                }
+                else
+                {
+                    currentPosition += skipSize;
+                }
+
+                skipSize += 1;
+            }
+            return elements[0] * elements[1];
+        }
+    }
+}
