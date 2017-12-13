f505b4bc40ce061f39ad16607d1e5efcb54e3e3c
Mjmarek <monicamarek24@gmail.com>
2017-12-12 22:56:49 -0500

    Day12b Challenge Solution

---
+++ Solutions.test/Day12aTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges;
+
+namespace Solutions.test
+{
+    [TestClass]
+    public class Day12aTest
+    {
+        [TestMethod]
+        public void ShouldReturn6()
+        {
+            var input = @"0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+
+            var result = new Day12a(input).Solve();
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultMonica()
+        {
+            var result = new Day12a(TestInputs.Day12Monica).Solve();
+
+            Assert.AreEqual(134, result);
+        }
+
+        [TestMethod]
+        public void SubmittedResultRyan()
+        {
+            var result = new Day12a(TestInputs.Day12Ryan).Solve();
+
+            Assert.AreEqual(380, result);
+        }
+    }
+}
---
+++ Solutions/Day12a.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day12a
+    {
+        private string input;
+        private List<string> visitedNeighbors;
+
+        public Day12a(string input)
+        {
+            this.input = input;
+            visitedNeighbors = new List<string>();
+        }
+
+        public int Solve()
+        {
+            var nodes = input
+                .Replace(",", "")
+                .Split('\n')
+                .Select(line => line.Split(' ').Select(item => item.Trim()).ToArray())
+                .ToDictionary(line => line[0], line => line.Skip(2));
+            var groupCount = 0;
+            while (nodes.Values.Count() > 0)
+            {
+                var neighbors =  FindNeighbors(nodes.First().Key, nodes).Distinct().Count();
+                foreach (var visited in visitedNeighbors)
+                {
+                    nodes.Remove(visited);
+                }
+                visitedNeighbors = new List<string>();
+                groupCount += 1;
+            }
+            return groupCount;
+        }
+
+        public List<string> FindNeighbors(string currentNode, Dictionary<string, IEnumerable<string>> nodes)
+        {
+            if (visitedNeighbors.Contains(currentNode)) return new List<string>();
+
+            var neighbors = nodes[currentNode];
+            var indirectNeighbors = new List<string>();
+            visitedNeighbors.Add(currentNode);
+            foreach (var neighbor in neighbors)
+            {
+                indirectNeighbors.AddRange(FindNeighbors(neighbor, nodes));
+            }
+            return neighbors.Concat(indirectNeighbors).ToList();
+        }
+    }
+}
