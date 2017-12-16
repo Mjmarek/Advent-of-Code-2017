b797185eb8a355196941b086c9cd776824d0b218
Mjmarek <monicamarek24@gmail.com>
2017-12-16 07:28:53 -0500

    WIP 10b challenge solution

---
+++ Solutions.test/Day10bTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenges;
+
+namespace Solutions.test
+{
+    [TestClass]
+    public class Day10bTest
+    {
+        //[TestMethod]
+        //public void ShouldReturnHash()
+        //{
+        //    var input = "1,2,3";
+
+        //    var result = new Day10b(input).Solve();
+
+        //    Assert.AreEqual("3efbe78a8d82f29979031a4aa0b16a9d", result);
+        //}
+
+        //[TestMethod]
+        //public void SubmittedResultMonica()
+        //{
+        //    var inputElementCount = 256;
+        //    var inputLengths = "212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164";
+
+        //    var result = new Day10b(inputElementCount, inputLengths).Solve();
+
+        //    Assert.AreEqual(212, result);
+        //}
+
+        //[TestMethod]
+        //public void SubmittedResultRyan()
+        //{
+        //    var inputElementCount = 256;
+        //    var inputLengths = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24";
+
+        //    var result = new Day10b(inputElementCount, inputLengths).Solve();
+
+        //    Assert.AreEqual(4480, result);
+        //}
+    }
+}
---
+++ Solutions/Day10b.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Day10b
+    {
+        private string input;
+        private List<char> lengths;
+        private int[] elements;
+        int currentPosition;
+        int skipSize;
+
+        public Day10b(string input)
+        {
+            this.input = input;
+        }
+
+        public int Solve()
+        {
+            var asciiCode = input.ToCharArray();
+            var suffix = new char[] { (char)17, (char)31, (char)73, (char)47, (char)23 };
+            var lengths = asciiCode.Concat(suffix).ToList();
+
+            var elements = Enumerable.Range(0, 256).ToArray();
+            var currentPosition = 0;
+            var skipSize = 0;
+
+            KnotHashPass(lengths, elements, ref currentPosition, ref skipSize);
+            return elements[0] * elements[1];
+        }
+
+        private static void KnotHashPass(List<char> lengths, int[] elements, ref int currentPosition, ref int skipSize)
+        {
+            foreach (var length in lengths)
+            {
+                var reverseList = new List<int>();
+
+                var currentReverseListPosition = currentPosition;
+                for (int i = 0; i < length; i++)
+                {
+                    if (currentReverseListPosition >= elements.Length)
+                    {
+                        currentReverseListPosition = 0;
+                    }
+                    reverseList.Add(elements[currentReverseListPosition]);
+
+                    currentReverseListPosition++;
+                }
+
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    if (currentPosition >= elements.Length)
+                    {
+                        currentPosition = 0;
+                    }
+                    elements[currentPosition] = reverseList[i];
+
+                    currentPosition++;
+                }
+
+                if (currentPosition + skipSize >= elements.Length)
+                {
+                    currentPosition = currentPosition + skipSize - elements.Length;
+                }
+                else
+                {
+                    currentPosition += skipSize;
+                }
+                skipSize += 1;
+            }
+        }
+    }
+}
